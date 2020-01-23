4e3b4375bc9f76599d1add9c65aa7b2681c6f903
tcallen45 <50537639+tcallen45@users.noreply.github.com>
2020-01-22 20:24:45 -0500

    Main java files for project

---
+++ League.java
@@ -0,0 +1,105 @@
+import java.awt.Image;
+import java.io.File;
+import java.util.ArrayList;
+
+public class League {
+    private String name;
+
+    private ArrayList<Team> teams;
+    
+    private String filename;
+    
+    private File logo;
+    
+    private String[] teamList;
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+    
+    public File getLogo() {
+        return this.logo;
+    }
+    
+    public void setLogo(String filename) {
+        this.logo = new File(filename);
+    }
+    
+    public void setTeams(ArrayList<Team> teams) {
+        this.teams = teams;
+    }
+    
+    public ArrayList<Team> getTeams(){
+        if(teams.size() == 0 || teams == null) {
+            return null;
+        }
+        else {
+            return teams;
+        }
+    }
+    
+    public final String[] getTeamList(){
+        String[] teamList = new String[this.teams.size()];
+        for(int i = 0; i < this.teams.size(); i++) {
+            teamList[i] = this.teams.get(i).getName();
+        }
+        return teamList;
+    }
+
+    @Override
+    public String toString() {
+        String retval = "";
+        if(teams.size() == 0 || teams == null) {
+            return "No Teams";
+        }
+        else {
+            for(int i = 0; i < teams.size(); i++) {
+                if(i != teams.size() - 1) {
+                    retval += teams.get(i) + ", ";
+                } else {
+                    retval += teams.get(i);
+                }
+                
+            }
+            return  name + " (" + retval + ")";
+        }
+            
+    }
+
+    public League(String name) {
+        this.name = name;
+        this.teams = null;
+        this.filename = null;
+        this.logo = new File(name);
+    }
+    
+    public League(String name, ArrayList<Team> teams) {
+        this.name = name;
+        this.teams = teams;
+        this.filename= null;
+        this.logo = new File(name);
+    }
+    
+    public static void main(String[] args) {
+        League test = new League("PL");
+        ArrayList<Team> team = new ArrayList<Team>();
+        Team arsenal = new Team("Arsenal");
+        Team city = new Team("City");
+        team.add(arsenal);
+        team.add(city);
+        test.setTeams(team);
+        System.out.println(test.getTeams());
+    }
+}
---
+++ Team.java
@@ -0,0 +1,193 @@
+import java.util.ArrayList;
+
+public class Team{
+    private String name;
+    
+    private String filename;
+
+    private int gamesPlayed;
+    
+    private int wins;
+    
+    private int losses;
+    
+    private int draws;
+    
+    private int points;
+    
+    private int goalsFor;
+    
+    private int goalsAgainst;
+    
+    private int goalDifference;
+    
+    private ArrayList<Match> matches;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    public void setGamesPlayed() {
+        this.gamesPlayed = matches.size();
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins() {
+        int tempWins = 0;
+        for(Match m : matches) {
+            if((this.name.equals(m.getHomeTeam().toString()) && m.getHomeResult().equals("W")) || (this.name.equals(m.getAwayTeam().toString()) && m.getAwayResult().equals("W"))) {
+                tempWins++;
+            }
+        }
+        this.wins = tempWins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses() {
+        int tempLosses = 0;
+        for(Match m : matches) {
+            if((this.name.equals(m.getHomeTeam().toString()) && m.getHomeResult().equals("L")) || (this.name.equals(m.getAwayTeam().toString()) && m.getAwayResult().equals("L"))) {
+                tempLosses++;
+            }
+        }
+        this.losses = tempLosses;
+    }
+
+    public int getDraws() {
+        return draws;
+    }
+
+    public void setDraws() {
+        int tempDraws = 0;
+        for(Match m : matches) {
+            if((this.name.equals(m.getHomeTeam().toString()) && m.getHomeResult().equals("D")) || (this.name.equals(m.getAwayTeam().toString()) && m.getAwayResult().equals("D"))) {
+                tempDraws++;
+            }
+        }
+        this.draws = tempDraws;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints() {
+        this.points = (this.wins * 3) + this.draws;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+    public int getGoalsFor() {
+        return goalsFor;
+    }
+
+    public void setGoalsFor() {
+        int tempGoalsFor = 0;
+        for(Match m : matches) {
+            if(this.name.equals(m.getHomeTeam().toString())) {
+                tempGoalsFor += m.getHomeScore();
+            } else if(this.name.equals(m.getAwayTeam().toString())) {
+                tempGoalsFor += m.getAwayScore();
+            }
+        }
+        this.goalsFor = tempGoalsFor;
+    }
+
+    public int getGoalsAgainst() {
+        return goalsAgainst;
+    }
+
+    public void setGoalsAgainst() {
+        int tempGoalsAgainst = 0;
+        for(Match m : matches) {
+            if(this.name.equals(m.getHomeTeam().toString())) {
+                tempGoalsAgainst += m.getAwayScore();
+            } else if(this.name.equals(m.getAwayTeam().toString())) {
+                tempGoalsAgainst += m.getHomeScore();
+            }
+        }
+        this.goalsAgainst = tempGoalsAgainst;
+    }
+
+    public int getGoalDifference() {
+        return goalDifference;
+    }
+
+    public void setGoalDifference() {
+        this.goalDifference = this.goalsFor - this.goalsAgainst;
+    }
+    
+    public ArrayList<Match> getMatches() {
+        return matches;
+    }
+    
+    public void addMatch(Match m) {
+        this.matches.add(m);
+    }
+    
+    public void removeMatch(Match m) {
+        ArrayList<Match> newMatches = new ArrayList<Match>();
+        for(Match game : getMatches()) {
+            if(!(m.equals(game))) {
+                newMatches.add(game);
+            }
+        }
+        this.matches = newMatches;
+    }
+
+    public void setMatches(ArrayList<Match> matches) {
+        this.matches = matches;
+    }
+
+    public Team(String name) {
+        this.name = name;
+        this.gamesPlayed = 0;
+        this.wins = 0;
+        this.draws = 0;
+        this.losses = 0;
+        this.points = 0;
+        this.goalsFor = 0;
+        this.goalsAgainst = 0;
+        this.goalDifference = 0;
+        this.matches = new ArrayList<Match>();
+    }
+    
+    public static void main(String[] args) {
+        Team test = new Team("Arsenal");
+        Match m = new Match("Arsenal", "Chelsea", 2, 1);
+        Match m2 = new Match("Brighton", "Arsenal", 1, 3);
+        Match m3 = new Match("Arsenal", "ManU", 2, 2);
+        test.addMatch(m);
+        test.addMatch(m2);
+        test.addMatch(m3);
+        //System.out.println(test.getMatches());
+        test.removeMatch(m2);
+        //System.out.println(test.getMatches());
+        
+    }
+}
